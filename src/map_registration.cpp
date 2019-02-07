a4f0ef6905b4b10920038594a2943c841e119c6c
zhangk22 <kzhang@mie.utoronto.ca>
2019-02-07 01:26:59 -0500

    icp with downsampled global cloud working

--- src/map_registration.cpp
+++ src/map_registration.cpp
@@ -21,9 +21,10 @@ class PointProcessor
         pcl::PointCloud<pcl::PointXYZI>::Ptr globalPointTCloud;
         bool first_msg;
         void icpCallback(const sensor_msgs::PointCloud2Ptr& msg);
-        void downSampleCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg);
+//        void downSampleCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg);
         void registerPair(pcl::PointCloud<pcl::PointXYZI>::Ptr newTCloud, pcl::PointCloud<pcl::PointXYZI>::Ptr globalCloud, Eigen::Matrix4f targetToSource);
         void performIcp(pcl::PointCloud<pcl::PointXYZI>::Ptr sourceTCloud, pcl::PointCloud<pcl::PointXYZI>::Ptr targetTCloud, Eigen::Matrix4f &outTransformation);
+        void downsampleTCloud(pcl::PointCloud<pcl::PointXYZI>::Ptr& inAndOutCloud);
         pcl::PointCloud<pcl::PointXYZI>::Ptr downSampleCloud(pcl::PointCloud<pcl::PointXYZI>::Ptr cloudIn);
 
         PointProcessor(){
@@ -35,43 +36,7 @@ class PointProcessor
 
         }
 };
-//callbacks
-void PointProcessor::downSampleCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
-{
-    pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2;
-    pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
-    pcl::PCLPointCloud2 cloud_filtered;
-
-    //Convert to PCL data type
-    pcl_conversions::toPCL(*cloud_msg, *cloud);
-
-    //Perform the actual filtering
-    //std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height
-    //  << " data points (" << pcl::getFieldsList (*cloud) << ").";
-
-    // Create the filtering object
-    pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-    sor.setInputCloud (cloudPtr);
-    sor.setLeafSize (0.1f, 0.1f, 0.1f);
-    sor.filter (cloud_filtered);
-    //std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height
-    //     << " data points (" << pcl::getFieldsList (*cloud_filtered) << ").";
-    sensor_msgs::PointCloud2 output_cloud;
-    pcl_conversions::fromPCL(cloud_filtered,output_cloud); //not pointer here?
-
-    pub.publish (output_cloud);
-
-}
-
-pcl::PointCloud<pcl::PointXYZI>::Ptr PointProcessor::downSampleCloud(pcl::PointCloud<pcl::PointXYZI>::Ptr cloudIn){
-    // Create the filtering object
-    pcl::VoxelGrid<pcl::PointXYZI> sor;
-    sor.setInputCloud (cloudIn);
-    sor.setLeafSize (0.05f, 0.05f, 0.05f);
-    pcl::PointCloud<pcl::PointXYZI>::Ptr filteredCloud (new pcl::PointCloud<pcl::PointXYZI>);
-    sor.filter (*filteredCloud);
-    return filteredCloud;
-}
+////callbacks
 
 void PointProcessor::registerPair(pcl::PointCloud<pcl::PointXYZI>::Ptr newTCloud, pcl::PointCloud<pcl::PointXYZI>::Ptr globalCloud, Eigen::Matrix4f targetToSource)
 {
@@ -93,25 +58,41 @@ void PointProcessor::performIcp(pcl::PointCloud<pcl::PointXYZI>::Ptr sourceTClou
     outTransformation = icp.getFinalTransformation() * outTransformation;
     std::cout << "getFinalTransformation \n"<< icp.getFinalTransformation() << std::endl;
 }
+
+void PointProcessor::downsampleTCloud(pcl::PointCloud<pcl::PointXYZI>::Ptr &inAndOutCloud)
+{    // Create the filtering object
+    pcl::PointCloud<pcl::PointXYZI>::Ptr tempFiltered (new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::VoxelGrid<pcl::PointXYZI> sor;
+    sor.setInputCloud (inAndOutCloud);
+    sor.setLeafSize (0.5f, 0.5f, 0.5f);
+    sor.filter (*tempFiltered);
+    *inAndOutCloud=*tempFiltered;
+}
+
 void PointProcessor::icpCallback(const sensor_msgs::PointCloud2Ptr& msg)
 {
     //initial pointcloud
     if (first_msg == true) {
         lastCloudMsg = *msg;
         pcl::fromROSMsg(*msg, *globalPointTCloud);
+        PointProcessor::downsampleTCloud(globalPointTCloud);  //downsample
         first_msg = false;
         return;
     }
 
-    if ((msg->header.stamp - lastCloudMsg.header.stamp).toSec() < 0.2) {
+    if ((msg->header.stamp - lastCloudMsg.header.stamp).toSec() < 0.5) {
         return;
     }
 
-    pcl::PointCloud<pcl::PointXYZI>::Ptr prevPointTCloud(new pcl::PointCloud<pcl::PointXYZI>);
-    pcl::fromROSMsg(lastCloudMsg, *prevPointTCloud); //void 	fromROSMsg (const sensor_msgs::PointCloud2 &cloud, pcl::PointCloud< T > &pcl_cloud)
+//    pcl::PointCloud<pcl::PointXYZI>::Ptr prevPointTCloud(new pcl::PointCloud<pcl::PointXYZI>);
+//    pcl::fromROSMsg(lastCloudMsg, *prevPointTCloud); //void 	fromROSMsg (const sensor_msgs::PointCloud2 &cloud, pcl::PointCloud< T > &pcl_cloud)
 
     pcl::PointCloud<pcl::PointXYZI>::Ptr PointTCloud(new pcl::PointCloud<pcl::PointXYZI>);
     pcl::fromROSMsg(*msg, *PointTCloud);
+
+    //downsample incoming pointcloud and return to the same variable
+    PointProcessor::downsampleTCloud(PointTCloud);
+
 
     //icp
     Eigen::Matrix4f Ti = Eigen::Matrix4f::Identity(), targetToSource;  //define 2 variables, Ti, targetToSource
@@ -122,19 +103,8 @@ void PointProcessor::icpCallback(const sensor_msgs::PointCloud2Ptr& msg)
     registerPair(PointTCloud, globalPointTCloud, targetToSource);
 
     //downsample global pointcloud
-    // Create the filtering object
-    pcl::VoxelGrid<pcl::PointXYZI> sor;
-    sor.setInputCloud (globalPointTCloud);
-    sor.setLeafSize (0.1f, 0.1f, 0.1f);
-    sor.filter (*globalPointTCloud);
+    PointProcessor::downsampleTCloud(globalPointTCloud);
 
-//    //global registration
-//    globalTargetToSource = globalTargetToSource*targetToSource;
-//    registerPair(PointTCloud, globalPointTCloud, globalTargetToSource);
-
-    //publish ROStopics
-//    pcl::toROSMsg(*prevPointTCloud, pairwiseCloudMsg);
-//    pairwise_pub.publish(pairwiseCloudMsg);
     pcl::toROSMsg(*globalPointTCloud, globalCloudMsg);
     registration_pub.publish(globalCloudMsg);
 
@@ -156,8 +126,8 @@ int main (int argc, char** argv){
     pointProcessor.pub=nh.advertise<sensor_msgs::PointCloud2> ("velodyne_points_downsampled", 1);
     pointProcessor.registration_pub=nh.advertise<sensor_msgs::PointCloud2> ("velodyne_points_aggregated", 1);
     pointProcessor.pairwise_pub=nh.advertise<sensor_msgs::PointCloud2> ("velodyne_points_pairwise", 1);
-    ros::Subscriber sub=nh.subscribe("velodyne_points", 1, &PointProcessor::downSampleCallback, &pointProcessor);
-    ros::Subscriber icpsub=nh.subscribe("velodyne_points_downsampled", 1, &PointProcessor::icpCallback, &pointProcessor);
+//    ros::Subscriber sub=nh.subscribe("velodyne_points", 1, &PointProcessor::downSampleCallback, &pointProcessor);
+    ros::Subscriber icpsub=nh.subscribe("velodyne_points", 1, &PointProcessor::icpCallback, &pointProcessor);
 
 
     while(ros::ok()){
